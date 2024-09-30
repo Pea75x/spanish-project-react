8ec4df7a57b6f2276ff20b93be7e5abdc13f7c98
Priya <priya_93_x@hotmail.co.uk>
2024-09-30 17:28:09 +0100

    login and signup switched to tailwind

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function FormInput({ label, ...otherProps }) {
   return (
-    <div className='field'>
-      {label && (
-        <label className='form-input-label' data-testid='label'>
-          {label}
-        </label>
-      )}
-      <input className='input' {...otherProps} data-testid='input' />
+    <div className='my-2'>
+      {label && <label data-testid='label'>{label}</label>}
+      <input
+        className='block w-full rounded-md border-0 p-2 text-gray-900 ring-1 ring-gray-300'
+        {...otherProps}
+        data-testid='input'
+      />
     </div>
   );
 }
