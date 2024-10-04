adf0380c71fdd4fc8d0d91e6b479fdffb2934879
Priya <priya_93_x@hotmail.co.uk>
2024-10-04 10:21:33 +0100

    added store

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { userId } from '../api/auth';
 import logo from '../logo.png';
 import ButtonsContainer from './ButtonsContainer';
+import { selectCurrentUser } from '../store/users/user.selector';
+import { useSelector } from 'react-redux';
 
 function MainPage() {
+  const currentUser = useSelector(selectCurrentUser);
   const [loggedIn, setLoggedIn] = React.useState(false);
+
+  console.log('current user:', currentUser);
 
   React.useEffect(() => {
     setLoggedIn(userId);
