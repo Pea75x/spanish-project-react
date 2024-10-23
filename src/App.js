aed10b2eb134f6d6237355afcd1af4f9205d7a9b
Priya <priya_93_x@hotmail.co.uk>
2024-10-23 14:58:40 +0100

    updated routes and removed games page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Homepage from './components/Homepage';
 import SignUp from './components/SignUp';
 import Words from './components/Words';
 import Sentences from './components/Sentences';
-import Games from './components/Games';
 import Word from './components/Word';
 import Create from './components/Create';
 
@@ -16,12 +15,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/game' element={<Game />} />
+        <Route path='/games' element={<Game />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/words' element={<Words />} />
         <Route path='/words-show' element={<Word />} />
         <Route path='/sentences' element={<Sentences />} />
-        <Route path='/games' element={<Games />} />
         <Route path='/create' element={<Create />} />
       </Routes>
     </BrowserRouter>
