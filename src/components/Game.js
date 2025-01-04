b08892b18c1d539aa7ae41299413911da980e32d
Priya <priya_93_x@hotmail.co.uk>
2025-01-04 00:07:43 +0000

    update format, need to complete game over logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,14 +11,14 @@ function Game() {
   const token = useSelector(selectToken);
   const { state } = useLocation();
   const { gameId } = state;
+  const [game, setGame] = React.useState({});
   const [sentences, setSentences] = React.useState([]);
-  const [playersSentences, setPlayersSentences] = React.useState([]);
   const [words, setWords] = React.useState(null);
   const [score, setScore] = React.useState(100);
   const [gameOver, setGameOver] = React.useState(false);
-  const [game, setGame] = React.useState({});
   const [gameScore, setGameScore] = React.useState({});
   const [message, setMessage] = React.useState('');
+  const [progress, setProgress] = React.useState(0)
 
   React.useEffect(() => {
     const getData = async () => {
@@ -32,7 +32,6 @@ function Game() {
         setGame(gameData);
         setSentences(allSentences.data);
         setWords(allWords.data);
-        setPlayersSentences(new Array(allSentences.data.length).fill([]));
       } catch (error) {
         console.log('error', error);
       }
@@ -40,40 +39,34 @@ function Game() {
     getData();
   }, []);
 
-  function getAnswers(answer, sentenceIndex) {
-    const updatedAnswer = playersSentences.map((sentence, i) =>
-      i === sentenceIndex ? answer : sentence
-    );
-
-    setPlayersSentences(updatedAnswer);
-  }
-
-  function markAnswers() {
+  function getAnswer(answer, sentenceIndex) {
     let currentScore = score;
-    let gameEnd = true;
-    const updatedSentences = sentences.map((sentence, sentenceIndex) => {
-      const updatedWords = sentence.words.map((word, wordIndex) => {
-        const playerAnswer = playersSentences[sentenceIndex][wordIndex];
+    let wrongAnswers = false;
+
+    const updatedWords = sentences[sentenceIndex].words.map((word, wordIndex) => {
+        const playerAnswer = answer[wordIndex];
         const isCorrect = playerAnswer === word.word;
         if (!isCorrect) {
           currentScore -= 1;
-          gameEnd = false;
+          wrongAnswers = true;
         }
         return {
           ...word,
           correct: isCorrect
         };
       });
-      return {
-        ...sentence,
-        words: updatedWords
-      };
-    });
-    setScore(currentScore);
-    setSentences(updatedSentences);
-    if (!!gameEnd) {
-      endGame(currentScore);
+
+    const updatedSentences = sentences.map((sentence, index) =>
+      index === sentenceIndex ? {...sentence, words: updatedWords} : sentence
+    );
+
+    setSentences(updatedSentences)
+    setScore(currentScore)
+
+    if (wrongAnswers === false) {
+      progress >= sentences.length - 1 ? endGame(currentScore) : setProgress(progress + 1)
     }
+    console.log(progress, sentences.length)
   }
 
   function endGame(score) {
@@ -97,11 +90,13 @@ function Game() {
   }
 
   return (
-    <div>
-      <h1 className='text-5xl font-bold text-center py-4'>{game.name}</h1>
-      <p className='text-xl text-center'>
-        Please fill in the boxes below with the spanish tanslation for each
-        sentence
+    <div className='bg-orange-100 py-4' style={{'height': 'calc(100vh - 50px)'}}>
+      {sentences && <div className='w-2/3 border border-black bg-white mx-auto my-2 rounded-xl h-[20px]'>
+        <div className={`h-full bg-amber-600 rounded-l-xl ${progress === 100 && 'rounded-xl'}`} style={{ width: `${(progress / sentences.length) * 100}%` }}></div>
+      </div>}
+      <h1 className='text-5xl font-bold text-center py-[20px] h-[90px]'>{game.name}</h1>
+      <p className='text-xl text-center h-[40px]'>
+        Please fill in the boxes below with the spanish tanslation
       </p>
       {message && (
         <div className='absolute w-full flex'>
@@ -110,29 +105,20 @@ function Game() {
           </div>
         </div>
       )}
+      <div className='flex justify-center items-center' style={{'height': 'calc(100vh - 230px)'}}>
+        {sentences[0] ? (
+          <div>
+            <SentenceCard sentence={sentences[progress]}
+              words={words}
+              markAnswer={getAnswer}
+              sentenceIndex={progress}/>
+            <div className="absolute bottom-0 right-0 p-6 text-5xl text-amber-600">{progress + 1}/{sentences.length}</div>
+          </div>
+        ) : (
+          <div>loading...</div>
+        )}
+      </div>
       {gameOver && <GameOver gameScore={gameScore} game={game} />}
-      {sentences ? (
-        <div className='flex flex-col items-center bg-amber-50 w-11/12 mx-auto rounded-lg shadow-lg my-4'>
-          {sentences.map((sentence, index) => (
-            <div key={sentence.id}>
-              <SentenceCard
-                sentence={sentence}
-                words={words}
-                getAnswers={getAnswers}
-                sentenceIndex={index}
-              />
-            </div>
-          ))}
-          <button
-            className='p-2 text-center hover:bg-amber-600 hover:text-white hover:border-amber-600 font-semibold border border-black rounded shadow ml-5 mb-5'
-            onClick={markAnswers}
-          >
-            Submit answers
-          </button>
-        </div>
-      ) : (
-        <div>Loading...</div>
-      )}
     </div>
   );
 }
